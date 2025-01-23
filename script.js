7775fd89d3e97e08e388ab7082466907ba2d4965
Sonvatan1453 <Sonvatan1453@hotmail.com>
2025-01-23 16:04:56 +0300

    first commit

--- script.js
+++ script.js
@@ -1,62 +1,108 @@
-var items = ['Vacuum the carpet', 'Sweep', 'Wash the dishes'];
+let todos = [];
+let editIndex = null;
 
-var list = document.querySelector('#myList');
+function renderTodos() {
+    const mete = document.getElementById("mete");
+    mete.innerHTML = todos
+        .map(
+            (todo) => `
+            <div class="todo-item">
+              <span>${todo.title}</span>
+              <div class="actions">
+                  <button onclick="editTodo('${todo.id}')">
+                      <i class="fa-solid fa-pencil"></i>
+                  </button>
+                  <button onclick="deleteTodo('${todo.id}')">
+                      <i class="fa-solid fa-trash"></i>
+                  </button>
+              </div>
+           </div>`
+        )
+        .join("");
+}
 
-items.forEach(function(item) {
-     
-});
+function fetchTodos() {
+    fetch("http://127.0.0.1:3000/getTodos")
+        .then((res) => res.json())
+        .then((res) => {
+            todos = res.payload;
+            renderTodos();
+        })
+        .catch((err) => {
+            alert("Görevler alınamadı: " + err.message);
+           
+        });
+}
 
+function addTodo() {
+    const newTodoInput = document.getElementById("newTodoInput");
+    const newTodo = newTodoInput.value.trim();
 
-list.addEventListener('click', function(item) {
-
-    if(item.target.tagName='li') {
-        item.target.classList.toggle('checked');
-    }
-
-});
-
-
-document.querySelector('#btnCreate').onclick = function(){;
-
-
-    var item = document.querySelector('#txtItem').value;
-
-    CreateItem(item);
-};
-
-function CreateItem(item){
-    var li = document.createElement('li');
-    var t = document.createTextNode(item);
-    li.className='list-group-item';
-    li.appendChild(t);
-    list.appendChild(li);
-
-
-    var span = document.createElement('span');
-    var text = document.createTextNode('\u00D7');
-    span.className="close";
-    span.appendChild(text);
-    li.appendChild(span);
-
-    span.onclick = function(){
-        var li = this.parentElement;
-        li.style.display="none";
+    if (newTodo) {
+        fetch("http://127.0.0.1:3000/addTodo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title: newTodo }),
+        })
+            .then((res) => res.json())
+            .then(() => {
+                fetchTodos();
+            })
+            .catch((err) => {
+                alert("Görev eklenemedi: " + err.message);
+                console.error(err);
+            });
+        newTodoInput.value = "";
     }
 }
-function editTask(element) {
-    const taskText = element.parentElement.querySelector('.task-text');
-    const newText = prompt('Edit task:', taskText.textContent);
-    if (newText !== null) {
-      taskText.textContent = newText;
-    }
-  };
 
-  function deleteTask(element) {
-    if (confirm('Silmek istediğinizden eminmisin?')) {
-      element.parentElement.remove();
+function deleteTodo(id) {
+    fetch(`http://127.0.0.1:3000/deleteTodo/${id}`, {
+        method: "DELETE",
+    })
+        .then((res) => res.json())
+        .then(() => {
+            fetchTodos();
+        })
+        .catch((err) => {
+            alert("Görev silinemedi: " + err.message);
+            console.error(err);
+        });
+}
+
+function editTodo(id) {
+    editIndex = id;
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        document.getElementById("editInput").value = todo.title;
     }
-  };
- 
-  
-  
-  
+}
+
+function saveTodo() {
+    const editInput = document.getElementById("editInput");
+    const updatedTodo = editInput.value.trim();
+
+    if (updatedTodo && editIndex) {
+        fetch(`http://127.0.0.1:3000/updateTodo/${editIndex}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title: updatedTodo }),
+        })
+            .then((res) => res.json())
+            .then(() => {
+                fetchTodos();
+                editIndex = null;
+            })
+            .catch((err) => {
+                alert("Görev güncellenemedi: " + err.message);
+                console.error(err);
+            });
+    }
+    editInput.value = "";
+}
+
+fetchTodos();
